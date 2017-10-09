54bd506e0dd3f8a1ef588aea7240b8a416881071
Ayodhyankit Paul <3x3paul@gmail.com>
2017-10-09 06:41:41 +0530

    Update code.py

--- code.py
+++ code.py
@@ -22,18 +22,20 @@ else:
 
     for i in qwerty:
 
-        if i in passcheck:
+        if i in passcheck:  #If entered password is in the form of 'QWERTY': subtract 15 points.
             points-=15
             print("your password contain form of 'QWERTY' , Don't use weak password.")
             print(points)
             sys.exit()
 
 if any(i.islower() for i in checking) or any(i.isupper() for i in checking) or any(i for i in checking if i in symbols) or any(i.isdigit() for i in checking):
-    points+=5
+    points+=5  #If there is at least one 'capital', 'lower case', 'symbol' or 'number': add 5 points.
 
 
 if any(i.islower() for i in checking) and any(i.isupper() for i in checking) and any(i.isdigit() for i in checking):
-    points+=15
+    points+=15 #If there is a capital and a number and a lower: add 15 points.
+
+
 
 print(points)
 
